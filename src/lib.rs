653cf0580c0d3f8291bf24a357f2c03254476f28
Warlock <internalmike@gmail.com>
2022-12-26 10:16:14 +0300

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![doc(test(attr(deny(warnings))))]
 #![doc(test(attr(allow(dead_code))))]
 #![doc(test(attr(allow(unused_variables))))]
+#![allow(clippy::type_complexity)]
 
 //! ## Feature flags
 #![doc=document_features::document_features!()]
