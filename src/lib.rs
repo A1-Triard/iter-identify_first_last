8433cd57734be76dc8c784e83590abeff4da72f7
Warlock <internalmike@gmail.com>
2022-12-26 09:44:42 +0300

    dw

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 #![cfg_attr(feature="nightly", feature(trusted_len))]
 
-#![no_std]
+#![deny(warnings)]
+#![doc(test(attr(deny(warnings))))]
+#![doc(test(attr(allow(dead_code))))]
+#![doc(test(attr(allow(unused_variables))))]
 
 #![doc=document_features::document_features!()]
 
 #[doc=include_str!("../README.md")]
 type _DocTestReadme = ();
+
+#![no_std]
 
 use core::fmt::{self, Debug, Formatter};
 use core::iter::{FusedIterator, Iterator, Map, Peekable};
