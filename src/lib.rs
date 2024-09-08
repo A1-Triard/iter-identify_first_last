c8e2de2769e5b7bbbd5284a04ee5ec47bece3a41
Warlock <internalmike@gmail.com>
2024-09-08 21:39:24 +0300

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![doc(test(attr(deny(warnings))))]
 #![doc(test(attr(allow(dead_code))))]
 #![doc(test(attr(allow(unused_variables))))]
+#![allow(clippy::needless_doctest_main)]
 #![allow(clippy::type_complexity)]
 
 //! ## Feature flags
